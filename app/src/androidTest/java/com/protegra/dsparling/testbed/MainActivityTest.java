421eace1ca07367e013609276597a5f8e6f516b9
unknown <dsparling@P0883-W8.protegra.com>
2014-12-16 12:26:08 -0600

    Add test case.

---
+++ app/src/androidTest/java/com/protegra/dsparling/testbed/MainActivityTest.java
@@ -0,0 +1,23 @@
+package com.protegra.dsparling.testbed;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.test.MoreAsserts.*;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    public void testHelloWorld() {
+        MainActivity main = getActivity();
+        
+        View helloWorld = main.findViewById(R.id.hello_world);
+        assertAssignableFrom(TextView.class, helloWorld);
+
+        String label = ((TextView) helloWorld).getText().toString();
+        assertContainsRegex("hello", label);
+    }
+}
