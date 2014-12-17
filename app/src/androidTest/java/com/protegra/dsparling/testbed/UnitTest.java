c5664c1c07539b90c74cc20ed75fadacd1c02170
unknown <dsparling@P0883-W8.protegra.com>
2014-12-17 14:04:25 -0600

    A vanilla unit test.

---
+++ app/src/androidTest/java/com/protegra/dsparling/testbed/UnitTest.java
@@ -0,0 +1,9 @@
+package com.protegra.dsparling.testbed;
+
+import junit.framework.TestCase;
+
+public class UnitTest extends TestCase {
+    public void testUnit() {
+        assertTrue("Look ma, no instrumentation!", 1 == 1);
+    }
+}
