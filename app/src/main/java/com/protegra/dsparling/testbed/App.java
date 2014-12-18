a2eab6185eadf0062c4d7d797a353960cd3935ea
unknown <dsparling@P0883-W8.protegra.com>
2014-12-18 14:17:21 -0600

    Work around stupid issue with Android Studio where it doesn't add the support lib dir in the SDK to the project's repositories.

---
+++ app/src/main/java/com/protegra/dsparling/testbed/App.java
@@ -0,0 +1,7 @@
+package com.protegra.dsparling.testbed;
+
+/**
+ * Created by dsparling on 2014-12-18.
+ */
+public class App {
+}
---
+++ app/src/main/java/com/protegra/dsparling/testbed/time/CurrentTimeService.java
@@ -0,0 +1,7 @@
+package com.protegra.dsparling.testbed.time;
+
+/**
+ * Created by dsparling on 2014-12-18.
+ */
+public class CurrentTimeService {
+}
